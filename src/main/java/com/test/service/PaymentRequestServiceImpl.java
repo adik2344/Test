b3eed70b5bc12130724ef1c7f15f74bb7bf3e6d2
adik2344 <adik2344@gmail.com>
2020-09-08 23:58:37 +0300

    add implementation of payment request service

---
+++ src/main/java/com/test/service/PaymentRequestServiceImpl.java
@@ -0,0 +1,29 @@
+package com.test.service;
+
+import com.test.domain.PaymentRequest;
+import com.test.domain.PaymentRequestStatus;
+import com.test.exception.NoSuchPaymentRequestException;
+import com.test.exception.RequestAlreadyExistException;
+import com.test.repository.PaymentRequestRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+
+//Implementation of {@link PaymentRequestService}
+
+@Service
+public class PaymentRequestServiceImpl implements PaymentRequestService {
+    private final PaymentRequestRepo paymentRequestRepo;
+
+    @Autowired
+    public PaymentRequestServiceImpl(PaymentRequestRepo paymentRequestRepo) {
+        this.paymentRequestRepo = paymentRequestRepo;
+    }
+}
