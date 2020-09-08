234e863a03baf91a2952722ced1a4ddf2903e401
adik2344 <adik2344@gmail.com>
2020-09-09 00:27:07 +0300

    add class of error DTO

---
+++ src/main/java/com/test/rest/dto/ErrorDTO.java
@@ -0,0 +1,12 @@
+package com.test.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDTO {
+    private String massage;
+}
