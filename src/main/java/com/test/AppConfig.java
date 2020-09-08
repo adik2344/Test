948b3eb2e7e07917ce1cb584d5487d77c8a1bff5
adik2344 <adik2344@gmail.com>
2020-09-08 23:39:33 +0300

    add App configuration class

---
+++ src/main/java/com/test/AppConfig.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
+
+
+//Configure application beans
+
+@Configuration
+public class AppConfig {
+
+}
